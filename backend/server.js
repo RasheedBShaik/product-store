9e4fe11bd0c8df9e79e3e94bf2ff12f8924e5bd8
rasheedbshaik <rasheedbasha.2004@gmail.com>
2025-11-12 12:04:16 +0530

    connect to user website

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: dev ? 'http://localhost:3000' : process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: dev ? 'http://localhost:3000' : 'https://product-store-zgez.onrender.com/', credentials: true }));
 app.use(express.json());
 
 // API routes
